7cb307e24af8dc8f2720fdc8b11f411a3e9cf9bd
vladislava_zh <vladislava_zh@malam.com>
2021-10-14 12:23:17 +0300

    server get random data

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
+const https = require('https')
 const app = express();
-
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/client-heroku'));
+var users = ''
 
-app.get('/*', (req, res) =>
+const options = {
+  hostname: 'randomuser.me',
+  path: '/api',
+  method: 'GET'
+}
+
+function getUsers() {
+
+  callback = function (response) {
+    var str = ''
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    response.on('end', function () {
+      users = str;
+    });
+  }
+
+  var req = https.request(options, callback);
+//This is the data we are posting, it needs to be a string or a buffer
+  req.write("hello world!");
+  req.end();
+  return users;
+}
+
+app.get('/', (req, res) =>
   res.sendFile('index.html', {root: 'dist/client-heroku/'}),
 );
+app.get("/random-users", (req, res, next) => {
+  var u = getUsers()
+  res.json(u)
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server running on port 8081");
+});
